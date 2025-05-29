4a9c6b0b049dff2403056520ee0a500b48c67b18
aryb086 <aryb086@gmail.com>
2025-05-28 21:52:28 -0700

    finished designing themes and added to file tree

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,21 @@ import 'package:congressional_app/features/authentication/screens/login.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'data/repositories/firebase_options.dart';
+import 'package:congressional_app/utils/theme/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
+    themeMode: ThemeMode.system,
+    theme: EAppTheme.light,
+    darkTheme: EAppTheme.dark,
     title: 'Eyes',
-    home: LoginPage(),
+    home: const LoginPage(),
   ));
 }
+
+
+
