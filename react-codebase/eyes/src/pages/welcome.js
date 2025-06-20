8f92f8de4148bf0af55c0fca7e3d32e18b96a715
aryb086 <aryb086@gmail.com>
2025-06-19 17:36:54 -0700

    built nav bar and initialized tailwind

---
+++ react-codebase/eyes/src/pages/welcome.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Welcome(){
+    return (
+        <div className='container'>
+            <h2>
+                Welcome to Eyes
+            </h2>
+            <p className='item'>
+                
+            </p>
+        </div>
+
+    )
+}
